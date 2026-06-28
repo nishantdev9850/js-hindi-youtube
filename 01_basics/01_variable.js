36d2252cb01129b88c3b3fc958c9a0c642f2f1d2
nishantbastapure <144418358+nishantbastapure@users.noreply.github.com>
2026-06-28 21:29:41 +0530

    javascript course start

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,24 @@
+const accountId = 144553;
+
+let accountEmail = "nishant.dev9850@gmail.com";
+
+var accountPassword = "nishantdev@9850";
+
+accountCity = "Pune";
+
+let accountState;
+
+// accountId = 9850; //Not allowed because accountId is a constant variable
+
+// console.log(accountId);
+
+accountEmail = "nishantbastapure19@gmail.com";
+// console.log(accountEmail);
+
+accountPassword = "nishantpass@9850";
+// console.log(accountPassword);
+
+accountCity = "Mumbai";
+accountState = "Maharashtra";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
